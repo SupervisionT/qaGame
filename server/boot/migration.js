ef7abb2e745c9c98b479252c4d08b3c42eefdec1
Abdallah <a.alshaikh82@gmail.com>
2020-10-26 20:04:48 +0300

    create user and store it in the User table

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -5,16 +5,36 @@ const answersSchema = require('../../common/models/answers.json');
 module.exports = async (app) => {
     const mysqlDs = app.dataSources.db;
     // migrate lb default tables
-    createLbTables(mysqlDs);
+    createLbTables(mysqlDs, app);
     // migrate models tables
     mysqlDs.autoupdate();
 };
 
-const createLbTables = (mysqlDs) => {
+const createLbTables = (mysqlDs, app) => {
     const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
     return mysqlDs.autoupdate(lbTables, function (er) {
         if (er) console.log(er);
         console.log(`Loopback tables [ ${lbTables} ] created in `, mysqlDs.adapter.name);
+        /**
+         * Create account
+         * @param {email} email
+         * @param {string} password
+         * To login use POST /Users/login
+         * with parameters:
+         * {
+         * "email": "a@b.com",
+         * "password": "Abcd1234"
+         * }
+         */
+        const account = {
+            email: 'a@b.com',
+            password: '$2a$10$y4A3PMabK3WVRQOzuVy.2u/T5NVdR7GksojimlmIyiggc1uBuFdQ.'
+        };
+        app.models.User.create(account, function (err, model) {
+            if (err) throw err;
+
+            console.log('Created:', model);
+        });
         return "db created"
     });
 }
